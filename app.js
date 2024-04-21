2a2ab6d9f90aa3727f88aaa0d66f1d89c489e3e9
Sepehr Yahyaee <7heycallmegray@gmail.com>
2024-04-21 22:09:28 +0330

    added morgan logger in it's most simplest form + public 404 not found middleware globally

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const logger = require('morgan');
 const { userRoutes, taskRoutes, tagRoutes } = require('./routes');
 const { errorHandler } = require('./middlewares');
 require('dotenv').config();
@@ -9,10 +12,13 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/task', taskRoutes);
 app.use('/api/tag', tagRoutes);
+app.use(logger('combined', { stream: fs.createWriteStream(path.join(__dirname, 'logFile.log'), { flags: 'a' }) }));
 app.use(errorHandler.globalErrorHandler);
 
 app.route('/').get((req, res, next) => {
     return res.send('Welcome to the Task Management App!');
 })
+
+app.all('*', (req, res) => {res.send('404 - NOT FOUND')});
 
 app.listen(process.env.PORT, () => { console.log('server is running on port: ', process.env.PORT) });
